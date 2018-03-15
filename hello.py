c96412c334a87d9353b5694a6a40c8926c50d84c
unknown <joaop.medeiros@gmail.com>
2018-03-14 22:33:26 -0300

    Ajuste texto

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World</hi>"
+    return "Hello World"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
