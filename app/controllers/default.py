8d8843608ff2df41c25a620b0001c9d951b4d95e
joao <joaop.medeiros@gmail.com>
2018-03-25 00:52:37 -0300

    Testando cadastro de usuario via heroku

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app,db
 from flask import render_template
+from app.models.forms import LoginForm
+from app.models.tables import Usuario
 
 # @app.route("/login", methods=["POST"])
 # def login():
@@ -7,15 +9,29 @@ from flask import render_template
 #     password = request.json.get('password')
 #     return jsonify({ 'nome': 'eitchaa' }), 201
 
-@app.route("/home")
-@app.route("/index/<user>")
-@app.route("/",defaults={"user":None})
-def index(user):
-    return render_template('index.html',user=user)
 
-@app.route("/base")
-def base():
-    return render_template('base.html')
+@app.route("/teste/<info>")
+@app.route("/teste",defaults={"info":None})
+def teste(info):
+    i = Usuario("gabriel.weich@bol.com.br",123456,"Gabriel Weich",123456)
+    db.session.add(i)
+    db.session.commit()
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form)
 
 # @app.route("/test")
 # @app.route("/test/<name>")
