791535755e25cf17448dac524c4c8409f7cc02c6
Joao Cecilio <joaop.medeiros@gmail.com>
2018-08-15 22:55:59 -0300

    AEEOOWWW

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -17,11 +17,11 @@ def load_user(id):
     return Usuario.query.filter_by(id=id).first()
 
 
-@app.route("/")
-def index():
-    if login_fresh():
-        return render_template('logado.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template('index.html')
+
 
 
 @app.route("/logado")
